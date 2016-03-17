7daceb1e795218355a5842095360bb70686a07bc
Michael Wolfendale <michael.wolfendale@gmail.com>
2016-03-17 10:52:54 +0000

    Added initial code

--- project/HmrcBuild.scala
+++ project/HmrcBuild.scala
@@ -1,43 +1,32 @@
-import sbt.Keys._
-import sbt._
-import uk.gov.hmrc.SbtAutoBuildPlugin
-import uk.gov.hmrc.versioning.SbtGitVersioning
-
-
-object HmrcBuild extends Build {
-
-  import BuildDependencies._
-  import uk.gov.hmrc.DefaultBuildSettings._
-
-  val appName = "play-whitelist-filter"
-
-  lazy val PlayWhitelistFilter = Project(appName, file("."))
-    .enablePlugins(SbtAutoBuildPlugin, SbtGitVersioning)
-    .settings(
-      targetJvm := "jvm-1.7",
-      libraryDependencies ++= Seq(
-        Test.scalaTest,
-        Test.pegdown
-      ),
-      Developers()
-    )
-}
-
-private object BuildDependencies {
-
-  object Compile {
-  }
-
-  sealed abstract class Test(scope: String) {
-    val scalaTest = "org.scalatest" %% "scalatest" % "2.2.4" % scope
-    val pegdown = "org.pegdown" % "pegdown" % "1.5.0" % scope
-  }
-
-  object Test extends Test("test")
-
-}
-
-object Developers {
-
-  def apply() = developers := List[Developer]()
-}
+import sbt._
+import sbt.Keys._
+import scoverage.ScoverageKeys
+import uk.gov.hmrc.SbtAutoBuildPlugin
+import uk.gov.hmrc.versioning.SbtGitVersioning
+
+object HmrcBuild extends Build {
+
+  import uk.gov.hmrc.DefaultBuildSettings._
+
+  val appName = "play-whitelist-filter"
+
+  lazy val PlayWhitelistFilter = Project(appName, file("."))
+    .enablePlugins(SbtAutoBuildPlugin, SbtGitVersioning)
+    .settings(
+      ScoverageKeys.coverageExcludedPackages := "uk.gov.hmrc.BuildInfo",
+      targetJvm := "jvm-1.8",
+      libraryDependencies ++= Seq(
+        "com.typesafe.play" %% "play" % "2.4.6",
+        "org.scalactic" %% "scalactic" % "2.2.6" % "test",
+        "org.scalatest" %% "scalatest" % "2.2.6" % "test",
+        "org.scalatestplus" %% "play" % "1.4.0" % "test",
+        "org.pegdown" % "pegdown" % "1.5.0" % "test"
+      ),
+      Developers()
+    )
+}
+
+object Developers {
+  def apply() = developers := List[Developer]()
+}
+
